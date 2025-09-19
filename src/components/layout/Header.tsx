493b981dc423ab49a2f5aea9e99023fdf0ee6aa6
Shyam Ruthvik Maddula <sruthvikmaddula@gmail.com>
2025-09-19 04:54:40 +0000

    remove standings options in the header (_for element div_)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -97,18 +97,10 @@ export default async function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
-        <div className="mr-auto md:flex">
+        <div className="mr-auto flex">
           <Logo />
         </div>
-        <nav className="flex items-center space-x-6 text-sm font-medium">
-          <Link
-            href="/"
-            className="transition-colors hover:text-foreground/80 text-foreground/60 flex items-center gap-1"
-          >
-            <LayoutGrid className="h-4 w-4" />
-            Standings
-          </Link>
-        </nav>
+        
         <div className="flex flex-1 items-center justify-end space-x-2">
           <ThemeToggle />
           <UserNav user={user} />
