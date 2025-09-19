0d2576ee2b783abb931932249ec18326a26ab6b0
Shyam Ruthvik Maddula <sruthvikmaddula@gmail.com>
2025-09-19 04:55:33 +0000

    make the entire header center it left oriented now (_for element header_

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -97,7 +97,7 @@ export default async function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
-        <div className="mr-auto flex">
+        <div className="flex items-center">
           <Logo />
         </div>
         
